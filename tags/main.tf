455f7e026b5681dd370034012d1889a6fca3a7e6
Aaron Conaway <40375373+aconaway1@users.noreply.github.com>
2022-05-05 09:27:52 -0400

    Init

---
+++ tags/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+  tags = {
+    WorkloadName       = "Training"
+    DataClassification = "Non-business"
+    Criticality        = "Low"
+    BusinessUnit       = "Shared"
+    OpsCommitment      = "Baseline only"
+    OpsTeam            = "NetEng"
+  }
+}
---
+++ tags/variables.tf
@@ -0,0 +1,7 @@
+variable "resource_group_name" {
+  default = "TF-RG"
+}
+
+variable "location" {
+  default = "australiacentral"
+}
